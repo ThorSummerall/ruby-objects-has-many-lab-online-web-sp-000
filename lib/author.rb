9324185d289617d9db3c1c0880a3aaa38e450355
Thor Summerall <thor.summerall@gmail.com>
2020-12-07 07:02:20 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,13 @@
 class Author
-  attr_accessor :name, :posts
+  attr_accessor :name, :post
+
+
 
   def initialize(name)
     @name = name
   end
 
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
 end
